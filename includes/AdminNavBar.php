d325579b5a6eae88a9b24151fe5de50a3bf9298c
jacobbndr <jacobbndr@gmail.com>
2015-12-11 15:50:16 -0500

    AdminNavBar patrh fixes

--- includes/AdminNavBar.php
+++ includes/AdminNavBar.php
@@ -7,15 +7,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/dallingn1/2014fall/projects/admin/admin.php">Home</a>
+      <a class="navbar-brand" href="/~dallingn1/2014fall/projects/admin/admin.php">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="../admin/cardatabase.php">Modify Cars </a></li>
-        <li><a href="../admin/QuestionMng.php">Modify Questions</a></li>
-		<li><a href="../admin/Users.php">Ban User</a></li>
+        <li class="active"><a href="/~dallingn1/2014fall/projects/admin/cardatabase.php">Modify Cars </a></li>
+        <li><a href="/~dallingn1/2014fall/projects/admin/QuestionMng.php">Modify Questions</a></li>
+		<li><a href="/~dallingn1/2014fall/projects/admin/Users.php">Ban User</a></li>
 		<li><a class="nav navbar-nav navbar-right" href="php/logout.php"> Log Out </a></li>
       </ul>
 	  
